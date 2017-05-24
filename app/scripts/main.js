b8957bb8fef008a3b4f5eac2cde19f4a4d549f5f
Sylvia Choong <sylvia.choong@loanstreet.com.my>
2017-05-24 12:59:15 +0800

    Add Text-To-Speech Form and API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	// update rate value
+	$(document).on('input', '#rate', function() {
+	  $('#rate_value').html( $(this).val() );
+	});
+
+	// voice selections
+	var voicelist = responsiveVoice.getVoices();
+	var i = 0;
+
+	do {
+	  $('#voice').append('<option>' + voicelist[i].name + '</option>')
+	  i++;
+	} while (i < voicelist.length);
+
+	// play voice
+	$('#text_form').submit(function(e){
+	  e.preventDefault();
+
+	  text = $('#text').val();
+	  voice = $('#voice').val();
+	  rate = $('#rate').val();
+
+	  responsiveVoice.speak(text, voice, {rate: rate});
+	});
+});
