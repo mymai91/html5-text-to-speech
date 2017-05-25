91b75166029d2c938e06d05075d86d81a58f35c9
Sylvia Choong <sylvia.choong@loanstreet.com.my>
2017-05-25 18:16:05 +0800

    Initial styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,26 @@ $(document).ready(function(){
 	});
 
 	// voice selections
-	var voicelist = responsiveVoice.getVoices();
+	var voicelist = ['UK English Female', 
+									'UK English Male', 
+									'Brazilian Portuguese Female', 
+									'Chinese Female', 
+									'Chinese (Hong Kong) Female',
+									'Deutsch Female',
+									'French Female',
+									'Hindi Female',
+									'Indonesian Female',
+									'Italian Female',
+									'Japanese Female',
+									'Korean Female',
+									'Polish Female',
+									'Russian Female',
+									'Spanish Female',
+									'Spanish Latin American Female']
 	var i = 0;
 
 	do {
-	  $('#voice').append('<option>' + voicelist[i].name + '</option>')
+	  $('#voice').append('<option>' + voicelist[i] + '</option>')
 	  i++;
 	} while (i < voicelist.length);
 
